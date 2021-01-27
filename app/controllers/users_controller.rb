af3c60b63b0a76eaa542545e624589b45eb58e39
Gabriel Gomes <gabrielogomes1@gmail.com>
2021-01-27 09:14:37 -0300

    Add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,23 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 2)
 
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+Article.create!([{
+  title: "Ant-Man",
+  description: "Armed with the astonishing ability .",
+  user_id: 1
+},
+{
+  title: "Pixels-Man",
+  description: "When aliens misinterpret video feeds of classic arcade games as a deo games.",
+  user_id: 1
+},
+{
+  title: "Terminator Genisys",
+  description: "When John Connorrn of euture...",
+  user_id: 1
+}])
+
+p "Created #{Article.count} movies"
